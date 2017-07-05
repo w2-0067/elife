71d5db4fd83589eb50c2034a270629535110ad60
w2-0067 <w2@m67.com>
2017-07-05 18:05:15 +0300

    05.07.17

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-
+  //open-menu
+  $('img.hamburger').click(function (){
+    $('.submenu').toggleClass('open-mob');
+    $('.mainmenu').toggleClass('open-mob');
+  });
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ gulp.task('concat', function () {
     .pipe(gulp.dest('app/css/'))
 	.pipe(reload({stream:true}));
   });
+  gulp.task('concat-m', function () {
+    return gulp.src('app/_media/*.css')
+    .pipe(concat('media.css'))
+    .pipe(gulp.dest('app/css/'))
+	.pipe(reload({stream:true}));
+  });
 gulp.task('browser-sync', function () {
   browserSync({
     server: {
@@ -19,6 +25,7 @@ gulp.task('browser-sync', function () {
 });
 gulp.task('watch', function () {
   gulp.watch('app/_css/*.css', ['concat']);
+  gulp.watch('app/_media/*.css' , ['concat-m']);
   gulp.watch('app/*.html' , reload);
   gulp.watch('app/js/*.js' , reload);
 });
